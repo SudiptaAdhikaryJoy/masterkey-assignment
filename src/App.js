0224abb42ef4bbae96d9bd65414d549f5123fa35
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2022-05-31 00:14:33 +0600

    footer design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Shared/Footer';
 
 function App() {
   return (
     <div className="App">
-      
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Shared/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+        <div class="container">
+            <div className="row">
+                <div className="col-md-3 col-lg-3">
+                    <h4>We are Social</h4>
+                    <h4>Follow Us</h4>
+                    <h1>LOGO</h1>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
